03325bacab2ac02104a3d5297c04747612eebffa
ImbaCOOL <imbacool@189.cn>
2020-04-30 12:08:20 +0800

    0

--- src/main.js
+++ src/main.js
@@ -6,17 +6,17 @@ import './style/base.css' // 引入样式重置
 // 引入element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import VueLazyload from 'vue-lazyload' // 引入图片懒加载
+// import VueLazyload from 'vue-lazyload' // 引入图片懒加载
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 // 图片懒加载
-Vue.use(VueLazyload, {
-  preLoad: 1.3,
-  error: '/favicon.ico',
-  loading: '/favicon.ico',
-  attempt: 1
-})
+// Vue.use(VueLazyload, {
+//   preLoad: 1.3,
+//   error: '/favicon.ico',
+//   loading: '/favicon.ico',
+//   attempt: 1
+// })
 
 new Vue({
   router,
