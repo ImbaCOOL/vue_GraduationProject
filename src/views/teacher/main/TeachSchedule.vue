7453f5d3fcf585ce5fc295c5437d018377ec57b1
ImbaCOOL <imbacool@189.cn>
2020-05-02 23:28:26 +0800

    0

--- src/views/teacher/main/TeachSchedule.vue
+++ src/views/teacher/main/TeachSchedule.vue
@@ -1,12 +1,50 @@
 <template>
   <div>
-      教师课表
+    <el-card class="card">
+      <div slot="header" class="clearfix">
+        <span>课程表</span>
+      </div>
+      <table border="1">
+        <tr>
+          <th>周一</th>
+          <th>周二</th>
+          <th>周三</th>
+          <th>周四</th>
+          <th>周五</th>
+        </tr>
+        <tr>
+          <td>row 1, cell 1</td>
+          <td>row 1, cell 2</td>
+          <td>row 1, cell 2</td>
+          <td>row 1, cell 2</td>
+          <td>row 1, cell 2</td>
+        </tr>
+        <tr>
+          <td>row 1, cell 1</td>
+          <td>row 1, cell 2</td>
+          <td>row 1, cell 2</td>
+          <td>row 1, cell 2</td>
+          <td>row 1, cell 2</td>
+        </tr>
+        <tr>
+          <td>row 1, cell 1</td>
+          <td>row 1, cell 2</td>
+          <td>row 1, cell 2</td>
+          <td>row 1, cell 2</td>
+          <td>row 1, cell 2</td>
+        </tr>
+        </table>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      a:""
+    }
+  },
 }
 </script>
 
