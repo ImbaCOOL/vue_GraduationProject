b22f31f38e0dd21d2815b64b82710e734ebc303b
ImbaCOOL <imbacool@189.cn>
2020-05-10 16:43:50 +0800

    0

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    productionSourceMap: false, // 把打包后的js.map文件删掉
-    publicPath: './', // 解析资源文件用相对路径
-    lintOnSave: false, // 关闭eslint
     devServer: {
         // 配置代理服务器
         proxy: { 
